eb530f7ab0781efc3f71431ad36606cd03beb6e9
David Karger <karger@mit.edu>
2020-06-16 17:28:24 -0400

    refactor to use signals.  requests mostly work

--- ifd.js
+++ ifd.js
@@ -1,101 +1,167 @@
-let unsubscribe=false;
+(function () {
 
-function watchRequests(sid,eid) {
-    if (console) {console.log(Mavo.value(sid), eid);}
-    function parseQuery(querySnapshot) {
-	/* object structure: 
-	   requests = {
-              label...: {
-	         count:
-	         names: []
-	         uids: {
-	            uid... : true
-	         }
-	      }
-	   }
-	*/
-	let requestMap={}, requestList=[]
-	, node=Mavo.Node.get(document.getElementById(eid));
+    fireAuth = new Promise((resolve,reject) => {
+	(function loop () {
+            if (typeof(firebase)!='undefined' && firebase.auth &&
+		firebase.apps.length > 0 && firebase.auth()) {
+		resolve(firebase.auth());
+            } else {
+		setTimeout(loop, 100)
+            }
+	})();
+    });
 
-	querySnapshot.forEach(function(doc) {
-	    let uid = doc.id;
-	    let name = doc.data().name;
-	    let theirs = doc.data().pick;
-	    theirs.forEach(pick => {
-		if (!requestMap[pick.label]) {
-		    requestMap[pick.label] = {count: 0, names: [], uids: {}};
-		}
-		let r=requestMap[pick.label];
-		if (!r.uids[uid]) {
-		    r.count++;
-		    r.names.push(name);
-		    r.uids[uid]=true;
-		}
-	    });
+    Promise.all([Mavo.inited,fireAuth]).then(([m,auth])=> {
+	let n=document.querySelector('[mv-app="ifdr"]'),
+	    storage='https://mavo-cd7c3.firebaseio.com/ifdr-user/'
+//	    n.getAttribute('mv-storage');
+
+	auth.onAuthStateChanged((user)=> {
+	    if (user) {
+		document.body.classList.remove('logged-out');
+		document.body.classList.add('logged-in');
+		n.setAttribute('mv-storage', storage+user.uid);
+	    } else {
+		document.body.classList.remove('logged-in');
+		document.body.classList.add('logged-out');
+		n.setAttribute('mv-source', storage+'0');
+	    }
+	    signal();
 	});
-	for (r in requestMap) {
-	    requestList.push({label: r, count: requestMap[r].count, names: requestMap[r].names});
+    });
+
+    let signalCounter=0
+    , signalNode
+    ;
+    setSignal = function(id) {
+	signalNode = Mavo.Node.get(document.getElementById(Mavo.value(id)));
+	return signalCounter;
+    }
+    function signal () {
+	if (signalNode) {
+	    signalNode.render(signalCounter++);
 	}
-	node.render({requests: requestList});
-	return {};
+    }
+    
+    uid = function() {
+	try {
+	    return firebase.auth().currentUser.uid;
+	}
+	catch (e) {
+	    return "";
+	}
+    }
+
+    username = function() {
+	try {
+	    return firebase.auth().currentUser.displayName;
+	}
+	catch (e) {
+	    return ""
+	}
+    }
+
+    let unsubscribe=false
+    , requests=[];
+    watchRequests = function(sid) {
+	fireAuth.then(() => {
+	    sid = Mavo.value(sid);
+	    if (console) {console.log(sid);}
+
+	    function parseQuery(querySnapshot) {
+		let requestMap={};
+		console.log('new requests')
+
+		querySnapshot.forEach(function(doc) {
+		    let uid = doc.id;
+		    let name = doc.data().name;
+		    let theirs = doc.data().pick;
+		    theirs.forEach(pick => {
+			if (!requestMap[pick.label]) {
+			    requestMap[pick.label] = {count: 0, names: [], uids: {}};
+			}
+			let r=requestMap[pick.label];
+			if (!r.uids[uid]) {
+			    r.count++;
+			    r.names.push(name);
+			    r.uids[uid]=true;
+			}
+		    });
+		});
+		requests=[];
+		for (r in requestMap) {
+		    requests.push({label: r,
+				   count: requestMap[r].count,
+				   names: requestMap[r].names});
+		}
+		signal();
+		return 0;
+	    };
+
+	    fireAuth.then(() => {
+		if (unsubscribe) {
+		    unsubscribe();
+		}
+		if (!sid) return;
+		
+		let query = firebase.firestore().collection('ifdr-user')
+		    .where('mysid','==',sid)
+		    .where('lastActive','>',Date.now()-4*60*60*1000); //4 hours
+ 		unsubscribe = query.onSnapshot(parseQuery);
+	    });
+	    return [];
+	})
     };
 
-    sid=Mavo.value(sid);
-    Promise.all([Mavo.inited,fireAuth]).then(() => {
-	if (unsubscribe) {
-	    unsubscribe();
+    getRequests = function (signal) {
+	return requests;
+    }
+
+    let sessions=[];
+    watchSessions = function() {
+	fireAuth.then(() => {
+	    let count=0;
+	    firebase.firestore().collection('ifdr-session').onSnapshot(shot=>{
+		sessions = [];
+		shot.forEach(doc => {
+		    let s=doc.data();
+		    s.id = doc.id;
+		    sessions.push(s);
+		});
+		signal();
+	    });
+	});
+    }
+
+    getSessions = function(signal) {
+	return sessions;
+    }
+
+    markPlayed = function(requests, played) {
+	let map={}, result=[];
+
+	try {
+	    played.forEach(p=> {
+		map[p.label]=true;
+	    })
+	    requests.forEach(r => {
+//		let s=Object.assign([],r);  //breaks mavo for unknown reasons
+		let s={label: Mavo.value(r.label),
+		       count: Mavo.value(r.count),
+		       names: Mavo.value(r.names),
+		       timestamp: Mavo.value(r.timestamp)};
+		for (k in s) {
+		    if (typeof s[k]=='undefined') {
+			delete s[k]
+		    }
+		}
+		s.played = map[r.label] ? true : false;
+		result.push(s);
+	    })
+	    return result;
+	} catch (e) {
+	    return [];
 	}
-	if (!sid || !eid) return;
 	
-	let query = firebase.firestore().collection('ifdr-user')
-	    .where('mysid','==',sid)
-	    .where('lastActive','>',Date.now()-4*60*60*1000); //4 hours
- 	unsubscribe = query.onSnapshot(parseQuery);
-    });
-    return [];
-};
-
-function markPlayed(requests, played) {
-    let map={}, result=[];
-
-    try {
-	played.forEach(p=> {
-	    map[p.label]=true;
-	})
-	requests.forEach(r => {
-	    //	    let s=Object.assign([],r);
-	    let s={label: Mavo.value(r.label),
-		   count: Mavo.value(r.count),
-		   names: Mavo.value(r.names)};
-	    s.played = map[r.label] ? true : false;
-	    result.push(s);
-	})
-	return {requests: result};
-    } catch (e) {
-	return [];
     }
-    
-}
 
-/*
-Idea: create a function f whose first argument is used for signaling.
-an invocation of f(null, x) tells f that the relevant mavo args have changed, meaning that f should reinitialize the observer and then signal an update (by rendering a new value on signal)
-an invocation of f(signal,x) is telling f that when its observed value changes, it should update signal.  f should also return the observed value 
-
-start with a function g(callback,...args) where the args initialize the observer and the callback is invoked with the new value whenever there is a change.
-*/
-function observer(init) {
-    let changes=0;
-    let currentValue;
-    let cache=null;
-    let signal=null;
-    
-    function callback(result) {
-    }
-    function observeFn(...args) {
-	if (args[0] == null) {
-	    init(callback,...args);
-	} else {
-	}
-    }
-}
